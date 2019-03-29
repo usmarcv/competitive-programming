776914aafc573d95dffd871f281c2e4dd9e05ec2
usmarcv <usmarc.v@gmail.com>
2019-03-28 22:56:46 -0400

    Insect!

---
+++ URIOnlineJudge/2862.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+  short c,n;
+
+  scanf("%hd",&c);
+
+  while (c--) {
+
+    scanf("%hd",&n);
+
+    if(n>8000){
+	    printf("Mais de 8000!\n");
+    }else printf("Inseto!\n");
+
+  }
+
+
+  return 0;
+}
