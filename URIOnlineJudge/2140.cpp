2dacd192bd53270c5412b438261b0158cda500db
usmarcv <usmarc.v@gmail.com>
2019-03-28 22:59:33 -0400

    Two Bills

---
+++ URIOnlineJudge/2140.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+
+  int N, M, i, restoNotas, duasNotas;
+  int notas[15] = {7,12,22,52,102,15,25,55,105,30,60,110,70,120,150};
+
+  for(;;){
+    scanf("%d %d",&N,&M);
+
+    if(N == 0 && M == 0)
+      break;
+      restoNotas = M - N; //erro o valor seria sempre negativo com N-M//////////////
+
+      for(i=0;i<15;i++) {
+        duasNotas=0;
+          if(notas[i] == restoNotas){
+            duasNotas = 1;
+            break;
+          }
+      }
+      if(duasNotas)
+        printf("possible\n");
+      else  printf("impossible\n");
+  }
+
+  return 0;
+
+}
