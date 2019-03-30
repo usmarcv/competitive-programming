a94472e63a70dbef1d494e18e060b81dc8d8277b
usmarcv <usmarc.v@gmail.com>
2019-03-30 16:18:47 -0400

    States from the North

---
+++ URIOnlineJudge/2803.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	string state;
+
+	cin >> state;
+
+	if(state.compare("acre") == 0){
+		cout << "Regiao Norte" << "\n";
+	}else if(state.compare("para") == 0){
+		cout << "Regiao Norte" << "\n";
+	}else if(state.compare("mato grosso") == 0){
+		cout << "Regiao Norte" << "\n";
+	}else if(state.compare("roraima") == 0){
+		cout << "Regiao Norte" << "\n";
+	}else if(state.compare("amazonas") == 0){
+		cout << "Regiao Norte" << "\n";
+	}else if(state.compare("amapa") == 0){
+		cout << "Regiao Norte" << "\n";
+	}else if(state.compare("tocantins") == 0){
+		cout << "Regiao Norte" << "\n";
+	}else if(state.compare("rondonia") == 0){
+		cout << "Regiao Norte" << "\n";
+	}else{
+		cout << "Outra regiao" << "\n";}
+
+	return 0;
+}
