edfc2c44463359e5cf8f5244ea7c011eb9575bb2
usmarcv <usmarc.v@gmail.com>
2019-04-01 17:10:28 -0400

    Number prime fast

--- notes/isPrimeFast.cpp
+++ notes/isPrimeFast.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 //Complexity of O(sqrt(n))
 bool isPrimeFast(ll n){
-	if(n < 0){n = -n}
+	if(n < 0){n = -n;}
 	if(n < 5 || (n % 2) == 0 || (n % 3) == 0){
 		return (n == 2 || n == 3);
 	}
