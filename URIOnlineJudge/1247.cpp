0a19ac021378b4af30a52106f92e976d705430c8
usmarcv <usmarc.v@gmail.com>
2019-04-23 15:26:24 -0400

    Coast Guard

---
+++ URIOnlineJudge/1247.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	double d, vf, vg, distance, distGuard = 0.0 , distThief = 0.0;
+	
+	while(scanf("%lf %lf %lf",&d, &vf, &vg) != EOF){
+				distance = sqrt(pow(d,2) + pow(12,2));	
+				distGuard = distance / vg;
+				distThief = 12 / vf;
+				if(distGuard > distThief) printf("N\n");
+				else printf("S\n");	
+	}
+
+	return 0;
+}
+
