38c912f3d4c429ea044c6d0336c08b361b8bf728
usmarcv <usmarc.v@gmail.com>
2019-04-24 11:14:27 -0400

    Dividing Circles

---
+++ URIOnlineJudge/2802.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	long long  n, ans;
+
+	scanf("%lld",&n);
+	ans = 1 + (((n * (n-1))/2) + (((n) * (n-1) * (n-2) * (n-3))/24));
+	printf("%lld\n",ans);
+
+	return 0;
+}
+
