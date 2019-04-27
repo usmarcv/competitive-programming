40e7c46495c513b275af86c6cc5fc0149d66d99e
usmarcv <usmarc.v@gmail.com>
2019-04-27 16:35:06 -0400

    Detective Watson

---
+++ URIOnlineJudge/1533.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+
+int main(){
+	
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int n;
+
+  while (cin >> n && n) {
+    vector <pair<short, short>> v(n);
+    for (int i = 0; i < v.size(); ++i) {
+  		    cin >> v[i].first;
+      		v[i].second = i + 1;
+    }
+    sort(v.rbegin(), v.rend());
+    cout << v[1].second << endl;
+	}
+
+	return 0;
+}
+
