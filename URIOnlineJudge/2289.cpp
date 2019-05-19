2916c5f8af96cad7016dbe39d88f5d5451db1a9d
usmarcv <usmarc.v@gmail.com>
2019-05-19 19:13:19 -0400

    Hamming Distance

---
+++ URIOnlineJudge/2289.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define endl '\n' 
+using namespace std;
+
+typedef unsigned long long ull;
+
+ull hammingDistance(ull x, ull y) {
+   return bitset<64>(x^y).count();
+}
+
+int main(){
+	
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	ull x, y;
+	x = y = 0ULL;
+
+	while((cin >> x >> y) && x != 0ULL){
+		
+		cout <<	hammingDistance(x,y) << endl;
+
+	}
+
+	return 0;
+}
+
