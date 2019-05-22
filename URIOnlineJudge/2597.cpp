8729b7a2b11abaab12fd493dd5a49106b6a14625
usmarcv <usmarc.v@gmail.com>
2019-05-22 09:15:19 -0400

    Xenlongão

---
+++ URIOnlineJudge/2597.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+using namespace std;
+
+ll n, c, ans;
+
+int main(){
+
+ 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+  
+  cin >> c;
+  while(c--){
+    cin >> n;
+    ans = sqrt(n);
+    ans = n - ans;
+    cout << ans << endl;
+  }
+
+	return 0;
+}
+
