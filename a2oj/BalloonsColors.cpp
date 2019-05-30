efc3f57bd7b7abf97944afd78b2488ae3eea3949
usmarcv <usmarc.v@gmail.com>
2019-05-29 22:42:13 -0400

    6. Balloons Colors

---
+++ a2oj/BalloonsColors.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl '\n' 
+#define __ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+
+int main(){__
+	
+	ll t, n, x, y;
+	short v[112];
+	cin >> t;
+	
+	while(t--){
+		
+		cin >> n >> x >> y;
+		
+		for(int i = 0; i < n; i++) cin >> v[i];
+		//primeira e última posição.
+		if(v[0] == x and v[n-1] == y) cout << "BOTH";
+		if(v[0] == x and v[n-1] != y) cout << "EASY";
+		if(v[0] != x and v[n-1] == y) cout << "HARD";
+		if(v[0] != x and v[n-1] != y) cout << "OKAY";
+		cout << endl; 
+
+	}
+
+	return 0;
+}
+
