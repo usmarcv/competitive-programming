86bfa093cbff33bd32e65892c3d067427f26c262
usmarcv <usmarc.v@gmail.com>
2019-06-21 22:56:47 -0400

    Counting Sheep

---
+++ uri/1609.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+using namespace std;
+
+int main(){_
+	
+	int casos, x, num, ans;
+	set <int> numeros;
+		
+	cin >> casos;
+	while(casos--){
+		cin >> x;
+		for(int i = 0; i < x; i++){
+				cin >> num;
+				numeros.insert(num);
+		}
+		ans = numeros.size();
+		cout << ans << endl;
+		ans = 0;
+		numeros.clear();
+	}	
+	return 0;
+
+}
+
