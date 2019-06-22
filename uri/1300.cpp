9579bba499b6ebb786580c402168ef4b0ad4d4a1
usmarcv <usmarc.v@gmail.com>
2019-06-21 22:29:19 -0400

    Hours and Minutes

---
+++ uri/1300.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+
+short num;
+
+int main(){_
+
+	while(cin >> num){
+		if(num % 6 == 0) printf("Y\n"); else printf("N\n");
+	}
+	
+	return 0;
+
+}
+
