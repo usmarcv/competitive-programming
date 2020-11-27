3f49c0286237b698fa30180564992d2d89c5b51b
Márcus Vinícius <usmarc.v@gmail.com>
2020-11-27 15:50:00 -0400

    Rest 1.0

---
+++ uri/3091.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define MOD 1000000007
+#define INF 0x3f3f3f3f
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+
+int main(){_
+
+	int a, b;
+	cin >> a >> b;
+	
+	cout << (a%b) << endl;
+	
+	return 0;
+
+}
+
