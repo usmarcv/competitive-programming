2fa9b485dd36dc717189aa49fb1a4047f40f0966
Márcus Vinícius <usmarc.v@gmail.com>
2020-11-27 15:57:47 -0400

    1068.cppParenthesis Balance I

---
+++ uri/1068.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+
+int main(){_
+
+	string s;
+	
+	while(cin >> s){
+	int esq = 0, dir = 0;
+		for(int i = 0; i < s.size(); i++){
+				if(s[i] == '(') esq++;
+				else if(s[i] == ')'){
+								dir++;
+								if(esq > 0){
+									esq--; 
+									dir--;
+								}
+				}
+		}
+
+		if(esq == 0 && dir == 0)	cout << "correct" << endl;
+		else cout << "incorrect" << endl;
+
+	}
+
+	return 0;
+
+}
+
