921825101730f7730bc60894f3eef6e8ff43ea36
Márcus Vinícius <usmarc.v@gmail.com>
2020-11-30 08:39:43 -0400

    Corrida

---
+++ uri/2416.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define MOD 1000000007
+#define INF 0x3f3f3f3f
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+
+int main(){_
+
+	int c, n;
+	
+	cin >> c >> n;
+
+	cout << (c%n) << endl;
+
+	return  0;
+
+}
+
