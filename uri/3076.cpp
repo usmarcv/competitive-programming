b010ada7668bcbebdeaafdd138f793de9f6d7704
Márcus Vinícius <usmarc.v@gmail.com>
2020-12-03 13:58:32 -0400

    History Exercise

---
+++ uri/3076.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define MOD 1000000007
+#define INF 0x3f3f3f3f
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+
+int main(){_
+
+	ull n, ans;
+
+	while(cin >> n){
+		ans = 0ULL;
+		ans = (ceil(n / 100.0));
+		cout << ans << endl;
+	}
+
+
+	return  0;
+
+}
+
