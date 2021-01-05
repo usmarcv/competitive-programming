1ed67e4b4f992864eed3c01ef2346140114f0b2a
Márcus Vinícius <usmarc.v@gmail.com>
2021-01-05 14:19:13 -0400

    Celebrity jeopardy

---
+++ uva/1124.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define MOD 1000000007
+#define INF 0x3f3f3f3f
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+
+int main(){_
+	
+	string s;
+
+	while(getline(cin,s)){
+		cout << s <<  endl;									
+	}
+
+	return  0;
+
+}
+
