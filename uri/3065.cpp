cec49c40be0e86ffb22a3dc30b79b2cbb0195615
Márcus Vinícius <usmarc.v@gmail.com>
2021-03-11 09:21:32 -0400

    update

---
+++ uri/3065.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define MOD 1000000007
+#define INF 0x3f3f3f3f
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+
+int main(){_
+
+	int n;
+	string s;
+
+	while(cin >> n && n){
+					int k, ans = 0;
+					for(int i = 0; i < k; i++){
+							cin >> s;
+							
+					}
+	}
+
+	return  0;
+
+}
+
