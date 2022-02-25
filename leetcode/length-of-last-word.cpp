6646824b613bdea6a96a69cddb6729a0db6b8c43
usmarcv <usmarc.v@gmail.com>
2022-02-25 14:27:07 -0300

    58. Length of Last Word

---
+++ leetcode/length-of-last-word.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+typedef long long ll;
+
+using namespace std;
+
+int main(){_
+
+	string s;
+
+	cin >> s;
+	
+	int ans = 0;
+	for(int i = 0; i < s.size()){
+			if(s[i] != ' '){
+				ans++;
+			}else{
+				else if(i < s.size() && s[i]==' '){
+				
+				}
+			}
+	}
+
+
+  return 0;
+}
+
+
