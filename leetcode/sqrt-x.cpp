a0c22ff2f4ea390e16d5eae2bb021da15d08c61b
Márcus Vinícius Lobo Costa <usmarc.v@gmail.com>
2022-07-15 12:27:49 -0300

    69. Sqrt(x)

---
+++ leetcode/sqrt-x.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+#define endl '\n'
+#define INF 0x3f3f3f3f
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+typedef long long ll;
+
+
+// -- Graph structure --
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+// ----------------------
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        return sqrt(x);
+    }
+};
+
+
+int main(){_
+	
+	Solution s;
+
+	int x;
+
+	cin >> x;
+
+	cout << s.mySqrt(x) << endl;
+
+	return 0;
+
+}
+
